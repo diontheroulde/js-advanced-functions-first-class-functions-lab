a39fad0ed645940419aeb50165980bc48a530f7a
Dion Theroulde <dionjr105@gmail.com>
2021-05-24 08:57:57 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  };
+  
+  const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+  
+  function createFareMultiplier(fareQuintupler) {
+      return function (integer) {
+          return fareQuintupler * integer;
+      }
+  }
+
+  function fareDoubler(doubleFare) {
+      return doubleFare * 2
+  }
+
+function fareTripler(tripleFare) {
+    return tripleFare * 3
+}
+
+function selectDifferentDrivers (drivers, returnDrivers) {
+    return returnDrivers (drivers)
+}
